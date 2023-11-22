0cb344a917c2c574e712da7dbb694e12bd983196
yupokok <kimberlywsp@gmail.com>
2023-11-22 14:10:03 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/ssf/day12/Day12Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.ssf.day12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/ssf/day12/Day12ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.ssf.day12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
